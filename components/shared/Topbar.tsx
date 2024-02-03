afb6663eca2831c3be62edd1b4e3de69d2ee671b
pananon <pandeyjss01@gmail.com>
2024-02-03 17:46:08 +0530

    Added sidebar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+
 export default function Topbar() {
   return (
-    <div>Topbar</div>
-  )
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image
+          src="/assets/Kindro-logos_white.png"
+          alt="logo"
+          width={34}
+          height={34}
+        />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Kindro</p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            // baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
 }
