276af65c33d9072c4469e7756e7b087c66d77c28
pananon <pandeyjss01@gmail.com>
2024-02-03 14:33:03 +0530

    Added components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +23,17 @@ export default function RootLayout({
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
         <html lang="en">
             <body className={`${inter.className} bg-dark-1`}>
-                {children}
+              <Topbar />
+              <main>
+                <LeftSidebar />
+                <section className="main-container">
+                  <div className="w-full max-w-4xl">
+                      {children}
+                  </div>
+                </section>
+                <RightSidebar />
+              </main>
+              <Bottombar/>
             </body>
         </html>
   </ClerkProvider>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
- 
 export default function Home() {
-  return (
-    <div className="h-screen">
-      <UserButton afterSignOutUrl="/"/>
-    </div>
-  )
+  return <h1 className="head-text text-left">Home</h1>;
 }
